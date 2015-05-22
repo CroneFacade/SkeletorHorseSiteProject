092a086b8e927c732ddcc4252e69a6bdba0059fd
Philip Bernhardsson <bernhardsson.philip@gmail.com>
2015-05-22 13:45:59 +0100

    Added Helpers for html text printing and string extensions for hashing.

---
+++ SkeletorHorseProject/SkeletorHorseProject/Helpers/HtmlHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkeletorHorseProject.Helpers
+{
+    public static class HtmlHelperClass
+    {
+        public static MvcHtmlString DisplayMultiline(this HtmlHelper helper, string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return MvcHtmlString.Empty;
+            }
+            return MvcHtmlString.Create(str.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None).Aggregate((a, b) => a + "<br />" + b));
+        }
+    }
+}
