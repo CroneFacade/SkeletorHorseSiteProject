fb0669db08dda920203c26bcdba27942968c9654
NellyHaglund <haglundnelly@gmail.com>
2015-05-22 15:20:40 +0200

    Created specific horse page with tab-toggling

---
+++ SkeletorHorseProject/SkeletorHorseProject/Controllers/HorseProfileController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkeletorHorseProject.Controllers
+{
+    public class HorseProfileController : Controller
+    {
+        // GET: HorseProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult HorseBlog()
+        {
+            return PartialView();
+        }
+    }
+}
