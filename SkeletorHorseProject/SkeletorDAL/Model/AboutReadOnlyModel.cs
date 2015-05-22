b915cc537b0fa4ba8e5ed0fd120b1636f809142a
Christopher <kung_cool_yes@hotmail.com>
2015-05-22 15:03:57 +0200

    ABout

---
+++ SkeletorHorseProject/SkeletorDAL/Model/AboutReadOnlyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletorDAL.Model
+{
+    public class AboutReadOnlyModel
+    {
+        public string Header1 { get; set; }
+        public string Header2 { get; set; }
+        public string Textfield1 { get; set; }
+        public string Textfield2 { get; set; }
+    }
+}
