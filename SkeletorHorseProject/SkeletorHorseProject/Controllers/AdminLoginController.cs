0c41be4593408cca4265ca5f42f605ea93014421
NellyHaglund <haglundnelly@gmail.com>
2015-05-22 13:30:22 +0200

    created admin login structure and page

---
+++ SkeletorHorseProject/SkeletorHorseProject/Controllers/AdminLoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SkeletorHorseProject.Models;
+
+namespace SkeletorHorseProject.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AdminLoginModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, false);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ SkeletorHorseProject/SkeletorHorseProject/Models/AdminLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SkeletorHorseProject.Models
+{
+    public class AdminLoginModel
+    {
+        [Required, MinLength(1), MaxLength(12)]
+        public string Username { get; set; }
+        [Required, MinLength(6), MaxLength(20)]
+        public string Password { get; set; }
+    }
+}
