f369b79505d84976eb788c70a0844add13c3caef
Robin Nilsson <robinonilsson@gmail.com>
2015-05-25 13:41:42 +0200

    Added Image to DB Poco classes

--- SkeletorHorseProject/SkeletorDAL/HorseContext.cs
+++ SkeletorHorseProject/SkeletorDAL/HorseContext.cs
@@ -14,7 +14,8 @@ namespace SkeletorDAL
 		public DbSet<Horse> Horses { get; set; }
 		public DbSet<Blog> Blogs { get; set; }
 		public DbSet<Post> Posts { get; set; }
-        public DbSet<About> Abouts { get; set; }
-	
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<GalleryImage> GalleryImages { get; set; }
+
 	}
 }
--- SkeletorHorseProject/SkeletorDAL/HorseContextInitializer.cs
+++ SkeletorHorseProject/SkeletorDAL/HorseContextInitializer.cs
@@ -14,7 +14,8 @@ namespace SkeletorDAL
 		private readonly List<Horse> horses;
 		private readonly List<Blog> blogs;
 		private readonly List<User> users;
-	    private readonly List<About> abouts; 
+		private readonly List<About> abouts;
+		private readonly List<GalleryImage> galleryImages;
 
 		public HorseContextInitializer()
 		{
@@ -23,8 +24,8 @@ namespace SkeletorDAL
 			var Gösta = new Horse("Gösta", DateTime.Now, "Camargue", 100, "-","100.000");
 			var Adolf = new Horse("Adolf", DateTime.Now, "Conny", 150, "Superduktig men har inte vunnit något","1 000 000");
 
-            Helge.IsForSale = true;
-		    Adolf.IsForSale = true;
+			Helge.IsForSale = true;
+			Adolf.IsForSale = true;
 
 
 
@@ -63,13 +64,13 @@ namespace SkeletorDAL
 				new User("admin", "admin".SuperHash(), 1)
 			};
 
-            abouts = new List<About>()
-            {
-                new About("Header1",@"balblablalblablalbla","Header2","heuheuehuehueheuheuhe"),
-                new About("Header1 Header up to date", 
-                          @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et enim quis sem blandit mollis eu sed nibh. Pellentesque eu neque erat. Nullam tempus purus velit, sed ullamcorper est suscipit ac. Aenean molestie odio ut purus tristique tempus. Suspendisse sed purus eget augue vulputate dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel fermentum nunc. Nam nec dapibus tortor. Cras tellus nibh, fringilla ut facilisis et, fringilla sit amet erat. In tempor bibendum turpis eget sollicitudin. Praesent pharetra rhoncus metus, a cursus massa consectetur pharetra. Nulla pulvinar nisi quis sem molestie, in luctus mi scelerisque. Nulla facilisi. Duis sit amet volutpat diam. Proin vitae auctor lectus.", "Header2 up to date", 
-                          @"Maecenas finibus viverra tincidunt. Praesent vitae est sed sem pharetra consequat. Phasellus facilisis lacus velit, id lacinia elit facilisis sit amet. Nullam luctus, nisi sit amet bibendum convallis, justo felis sodales eros, nec rutrum tellus sapien ornare ex. Morbi fermentum tristique magna ut hendrerit. Mauris tincidunt ante ut libero mollis, eu pulvinar nunc dapibus. Fusce vestibulum nibh dui, ut blandit nisl pellentesque eu. In id sapien eget neque suscipit sollicitudin at quis augue. Quisque ac eleifend leo, in porta dolor. Pellentesque luctus lectus sapien, a pulvinar velit faucibus quis. Curabitur a mattis eros. Aenean lorem turpis, accumsan nec consequat in, tincidunt vitae velit. Nulla quis tellus convallis, ullamcorper purus at, pellentesque ligula.")
-            };
+			abouts = new List<About>()
+			{
+				new About("Header1",@"balblablalblablalbla","Header2","heuheuehuehueheuheuhe"),
+				new About("Header1 Header up to date", 
+						  @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et enim quis sem blandit mollis eu sed nibh. Pellentesque eu neque erat. Nullam tempus purus velit, sed ullamcorper est suscipit ac. Aenean molestie odio ut purus tristique tempus. Suspendisse sed purus eget augue vulputate dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel fermentum nunc. Nam nec dapibus tortor. Cras tellus nibh, fringilla ut facilisis et, fringilla sit amet erat. In tempor bibendum turpis eget sollicitudin. Praesent pharetra rhoncus metus, a cursus massa consectetur pharetra. Nulla pulvinar nisi quis sem molestie, in luctus mi scelerisque. Nulla facilisi. Duis sit amet volutpat diam. Proin vitae auctor lectus.", "Header2 up to date", 
+						  @"Maecenas finibus viverra tincidunt. Praesent vitae est sed sem pharetra consequat. Phasellus facilisis lacus velit, id lacinia elit facilisis sit amet. Nullam luctus, nisi sit amet bibendum convallis, justo felis sodales eros, nec rutrum tellus sapien ornare ex. Morbi fermentum tristique magna ut hendrerit. Mauris tincidunt ante ut libero mollis, eu pulvinar nunc dapibus. Fusce vestibulum nibh dui, ut blandit nisl pellentesque eu. In id sapien eget neque suscipit sollicitudin at quis augue. Quisque ac eleifend leo, in porta dolor. Pellentesque luctus lectus sapien, a pulvinar velit faucibus quis. Curabitur a mattis eros. Aenean lorem turpis, accumsan nec consequat in, tincidunt vitae velit. Nulla quis tellus convallis, ullamcorper purus at, pellentesque ligula.")
+			};
 		}
 
 		public override void InitializeDatabase(HorseContext context)
@@ -77,7 +78,8 @@ namespace SkeletorDAL
 			horses.ForEach(h => context.Horses.Add(h));
 			blogs.ForEach(b => context.Blogs.Add(b));
 			users.ForEach(u => context.Users.Add(u));
-		    abouts.ForEach(a => context.Abouts.Add(a));
+			abouts.ForEach(a => context.Abouts.Add(a));
+            galleryImages.ForEach(i => context.GalleryImages.Add((i)));
 			base.InitializeDatabase(context);
 		}
 	}
