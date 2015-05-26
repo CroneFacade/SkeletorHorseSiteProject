8b6e24c71851e8e142b989368d7228e1e54e8253
theresesango <therese.sangoe@gmail.com>
2015-05-26 14:54:55 +0200

    _layout and footer

---
+++ SkeletorHorseProject/SkeletorHorseProject/Controllers/FooterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkeletorDAL.Model;
+
+namespace SkeletorHorseProject.Controllers
+{
+    public class FooterController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            List<FooterModel> list = new List<FooterModel>()
+            {
+                new FooterModel()  {
+                Name = "Triton",
+                Url = "blogspot.com"
+            },
+             new FooterModel()  {
+                Name = "Leila",
+                Url = "blogspot.com"
+            },
+             new FooterModel()  {
+                Name = "Flilip",
+                Url = "blogspot.com"
+            },
+             new FooterModel()  {
+                Name = "Stoffe",
+                Url = "blogspot.com"
+            },
+
+            };
+            
+            return PartialView("Footer", list);
+        }
+    }
+}
