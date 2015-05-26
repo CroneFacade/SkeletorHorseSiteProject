7bc3b4591930cf19b3051eb24171d83c80ae9928
Robin Nilsson <robinonilsson@gmail.com>
2015-05-26 15:24:22 +0200

    added facebook page, wrong format, trying to fix later

--- SkeletorHorseProject/SkeletorDAL/Repository.cs
+++ SkeletorHorseProject/SkeletorDAL/Repository.cs
@@ -51,6 +51,7 @@ namespace SkeletorDAL
                             Medicine = h.Medicine,
                             FamilyTree = h.FamilyTree,
                             Awards = h.Awards
+                        
                         }).FirstOrDefault();
             }
         }
