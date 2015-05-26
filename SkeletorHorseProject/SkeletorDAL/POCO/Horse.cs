cb419fae638c5de7ec8c506ed1bb878430f307dc
JHNWLL <johanwall1@hotmail.com>
2015-05-26 11:04:50 +0200

    added things to horsetable in database

--- SkeletorHorseProject/SkeletorDAL/POCO/Horse.cs
+++ SkeletorHorseProject/SkeletorDAL/POCO/Horse.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
+using SkeletorDAL.POCO;
 
 namespace SkeletorDAL
 {
@@ -19,6 +20,10 @@ namespace SkeletorDAL
 		public bool IsForSale { get; set; }
 		public string Price { get; set; }
 	    public bool IsSold { get; set; }
+		public string Description { get; set; }
+		public string Medicine { get; set; }
+		public string FamilyTree { get; set; }
+		public string ImagePath { get; set; }
 
 	    public Horse()
 		{
