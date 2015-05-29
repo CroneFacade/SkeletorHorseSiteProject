25b34b1da73fc282defb6b8e9a4e84815d327c39
NellyHaglund <haglundnelly@gmail.com>
2015-05-29 09:50:05 +0200

    Added [Column(TypeName=datetime2)] to fix sql problem

--- SkeletorHorseProject/SkeletorDAL/Model/HorseProfileModel.cs
+++ SkeletorHorseProject/SkeletorDAL/Model/HorseProfileModel.cs
@@ -9,11 +9,22 @@ namespace SkeletorDAL.Model
 {
     public class HorseProfileModel
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
         public string Race { get; set; }
         public int Withers { get; set; }
         public string Awards { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsForSale { get; set; }
+        public string Price { get; set; }
+        public bool IsSold { get; set; }
+        public string State { get; set; }
+        public string Description { get; set; }
+        public string Medicine { get; set; }
+        public string FamilyTree { get; set; }
+        public string ImagePath { get; set; }
         public string FacebookPath { get; set; }
+        public Blog Blog { get; set; }
     }
 }
