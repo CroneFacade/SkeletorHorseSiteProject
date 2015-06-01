b7d12bd6a7617226dba27746342126f4f50a6c94
Philip Bernhardsson <bernhardsson.philip@gmail.com>
2015-06-01 13:44:20 +0100

    Added model validation to youtube urls for horses

--- SkeletorHorseProject/SkeletorDAL/Model/HorseVideoModel.cs
+++ SkeletorHorseProject/SkeletorDAL/Model/HorseVideoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace SkeletorDAL.Model
     public class HorseVideoModel
     {
         public int ID { get; set; }
+        [Required]
         public string VideoName { get; set; }
+        [Required, Url]
         public string VideoURL { get; set; }
         public int HorseID { get; set; }
     }
