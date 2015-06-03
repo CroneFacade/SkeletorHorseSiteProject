80c9d6e80578205887df5bc6025370729b13eb30
RojjeMedEttJ <rojehaddad@gmail.com>
2015-06-03 12:55:11 +0200

    fixed admin issue

---
+++ SkeletorHorseProject/SkeletorDAL/Helpers/AutoCompleteSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeletorDAL.Helpers
+{
+  public  class AutoCompleteSearch
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
