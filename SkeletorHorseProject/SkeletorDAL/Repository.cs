61623abbd1390fcd2acc5c76e500b5f378b3a1d8
JHNWLL <johanwall1@hotmail.com>
2015-06-04 10:01:18 +0200

    added gender to filteredhorsepage

--- SkeletorHorseProject/SkeletorDAL/Repository.cs
+++ SkeletorHorseProject/SkeletorDAL/Repository.cs
@@ -392,7 +392,8 @@ namespace SkeletorDAL
                                          IsSold = h.IsSold,
                                          Breeding = h.Breeding,
                                          State = "All horses",
-                                         ImagePath = h.ImagePath
+                                         ImagePath = h.ImagePath,
+										 Gender = h.Gender
                                      }).ToList();
                         break;
 
@@ -413,7 +414,8 @@ namespace SkeletorDAL
                                          Withers = h.Withers,
                                          IsSold = h.IsSold,
                                          State = "Horses for sale",
-                                         ImagePath = h.ImagePath
+                                         ImagePath = h.ImagePath,
+										 Gender = h.Gender
                                      }).ToList();
                         break;
                     case 3:
@@ -433,7 +435,8 @@ namespace SkeletorDAL
                                          Withers = h.Withers,
                                          IsSold = h.IsSold,
                                          State = "Sold horses",
-                                         ImagePath = h.ImagePath
+                                         ImagePath = h.ImagePath,
+										 Gender = h.Gender
                                      }).ToList();
                         break;
                 }
