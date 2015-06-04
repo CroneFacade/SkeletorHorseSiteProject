3bcef6ebb7cb6fcb38cdb1c68f83aa74188f5622
RojjeMedEttJ <rojehaddad@gmail.com>
2015-06-04 11:35:46 +0200

    added authorization for register admin

--- SkeletorHorseProject/SkeletorHorseProject/Controllers/RegisterAdminController.cs
+++ SkeletorHorseProject/SkeletorHorseProject/Controllers/RegisterAdminController.cs
@@ -9,6 +9,7 @@ using SkeletorDAL.Model;
 namespace SkeletorHorseProject.Controllers
 {
 
+    [Authorize]
     public class RegisterAdminController : Controller
     {
         // GET: RegisterAdmin
